227a6b7d117e9132ef234f3d250ad2d884d6707c
Soham Shiraskar <120788848+Shiraskar-Soham@users.noreply.github.com>
2023-01-13 13:28:40 +0530

    Create reverse_asterisk_triangle.cpp

---
+++ PracticeQuestions/reverse_asterisk_triangle.cpp
@@ -0,0 +1,14 @@
+/*reverse asterisk triangle*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+   cout<<"enter a number: ";
+   cin>>n;
+   for (int i=n;i>=1; i--){
+       for(int j=i; j>=1; j--){
+           cout<<"* ";
+       }
+       cout<<endl;
+   }
+}
